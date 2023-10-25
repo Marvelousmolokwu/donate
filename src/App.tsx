91f34f2ac5cabc1e65d6a076a8c2fd0d7895f5d6
Marvelousmolokwu <qmolokwu@gmail.com>
2023-10-25 15:07:14 -0700

    trying to fix the bug trial 5

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 
-// import { Form1 } from "./pages/Registration/form1"
+import { Form1 } from "./pages/Registration/form1"
 
-// import { Form2 } from "./pages/Registration/form2"
+import { Form2 } from "./pages/Registration/form2"
 
-// import { Login } from "./pages/Registration/login"
+import { Login } from "./pages/Registration/login"
 import { ForgotPassword } from "./pages/Registration/forgotPassword"
 import { useLoader } from "./hooks/useLoader"
 import { Loader } from "./components/loader/loader"
@@ -24,11 +24,11 @@ function App() {
     {isloading ? <Loader/>:     <BrowserRouter>
       <Routes>
   
-  <Route path="/home" element={<React.Suspense fallback={<Loader/>}><LazyAbout /></React.Suspense>} />
-  {/* <Route path={"/:user/form1"} element={<Form1  />}/>
-  <Route path={"/:user/form2"} element={<Form2/>}/>
-  <Route path="/login" element={<Login/>}/> */}
-  <Route path="/" element={<ForgotPassword/>}/>
+  <Route path="/" element={<React.Suspense fallback={<Loader/>}><LazyAbout /></React.Suspense>} ></Route>
+  <Route path={"/:user/form1"} element={<Form1  />}></Route>
+  <Route path={"/:user/form2"} element={<Form2/>}></Route>
+  <Route path="/login" element={<Login/>}></Route>
+  <Route path="/forgotpassword" element={<ForgotPassword/>}></Route>
   
       </Routes>
       
