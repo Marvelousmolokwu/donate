0266ee672c834bcbd78c113d8ee65065775e64b0
Marvelousmolokwu <qmolokwu@gmail.com>
2023-10-26 03:36:05 -0700

    trying to fix the bug trial 7

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,21 @@ import { Form2 } from "./pages/Registration/form2"
 
 import { Login } from "./pages/Registration/login"
 import { ForgotPassword } from "./pages/Registration/forgotPassword"
-import { useLoader } from "./hooks/useLoader"
-import { Loader } from "./components/loader/loader"
-// lazyload for homepage
+// import { useLoader } from "./hooks/useLoader"
+// import { Loader } from "./components/loader/loader"
+// // lazyload for homepage
 const LazyAbout = React.lazy(()=> import("./pages/home/home.page"))
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
 
 
 
 function App() {
-  const { isloading} = useLoader()
+  // const { isloading} = useLoader()
   const router = createBrowserRouter(
 createRoutesFromElements(
   <Route>
   
-  <Route index element={<React.Suspense fallback={<Loader/>}><LazyAbout /></React.Suspense>} ></Route>
+  <Route index element={<React.Suspense fallback={"loading"}><LazyAbout /></React.Suspense>} ></Route>
   <Route path={"/:user/form1"} element={<Form1  />}/>
   <Route path={"/:user/form2"} element={<Form2/>}/>
   <Route path="login" element={<Login/>}/>
@@ -35,7 +35,9 @@ createRoutesFromElements(
 
   return (
     <>
-    {isloading ? <Loader/>:  <RouterProvider router={router}/>}  
+    {/* {/* {isloading ? <Loader/>:   */}
+    
+    <RouterProvider router={router}/>
 
     
         
