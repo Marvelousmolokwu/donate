cde38986e41e797d3a073fc577a3ddc9f28b009e
Marvelousmolokwu <qmolokwu@gmail.com>
2023-10-26 05:23:28 -0700

    trying to fix the bug trial 9

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ import { Form1 } from "./pages/Registration/form1"
 import { Form2 } from "./pages/Registration/form2"
 
 import { Login } from "./pages/Registration/login"
-import { ForgotPassword } from "./pages/Registration/forgotPassword"
+import { ForgotPassword } from "./pages/Registration/forgotpassword/forgotPassword"
 // import { useLoader } from "./hooks/useLoader"
 // import { Loader } from "./components/loader/loader"
 // // lazyload for homepage
-const LazyAbout = React.lazy(()=> import("./pages/home/home.page"))
+const LazyAbout = React.lazy(()=> import("./pages/home"))
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
 
 
@@ -22,7 +22,7 @@ function App() {
 createRoutesFromElements(
   <Route>
   
-  <Route path="/home" element={<React.Suspense fallback={"loading"}><LazyAbout /></React.Suspense>} ></Route>
+  <Route path="home" element={<React.Suspense fallback={"loading"}><LazyAbout /></React.Suspense>} ></Route>
   <Route path={"/:user/form1"} element={<Form1  />}/>
   <Route path={"/:user/form2"} element={<Form2/>}/>
   <Route path="login" element={<Login/>}/>
